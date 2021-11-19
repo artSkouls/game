927414a2bb321711b653ca1c1eef9b0a7fc11158
Archi <arturskouls@gmail.com>
2021-11-19 13:10:38 +0300

    add ball

--- pin_pong.py
+++ pin_pong.py
@@ -2,13 +2,26 @@ import time
 import os
 import pynput
 
+ballX = 7
+ballY = 7
+width = 30
+height = 10
+
 def draw():
+    y = 0
+    while y < height:
+        x = 0
+        result = ""
+        while x < width:
+            if x == round(ballX) and y == round(ballY):
+                result += 'o'
+            else:
+                result += " "
+            x += 1
 
+        print(result)
+        y += 1
 
-def press_instruction(key):
-
-
-def release_instruction(key):
 
 
 
