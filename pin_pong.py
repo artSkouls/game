836ea5ccb1129ca0b4ad0514dafd566ddc45fc39
Archi <arturskouls@gmail.com>
2021-11-19 13:06:33 +0300

    add functions

---
+++ pin_pong.py
@@ -0,0 +1,20 @@
+import time
+import os
+import pynput
+
+def draw():
+
+
+def press_instruction(key):
+
+
+def release_instruction(key):
+
+
+
+
+
+while True:
+    os.system('clear')
+    draw()
+    time.sleep(0.1)
